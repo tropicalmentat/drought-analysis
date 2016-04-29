202ab3d869c08c625550ddd63c1c4d57e1e37f88
tropicalhobo <jose.torres@obf.ateneo.edu>
2016-04-29 10:50:48 +0800

    fixed workflow

--- collect-pixels.py
+++ collect-pixels.py
@@ -21,6 +21,8 @@ import matplotlib
 def new_csv():
     pass
 
+def map_to_pix():
+    pass
 
 def collect_pixels(fn, r_dir):
 
@@ -48,10 +50,21 @@ def collect_pixels(fn, r_dir):
         path, basename = os.path.split(r)
         if '2015' in basename or '2016' in basename:  # ignore files not included in time period
             print basename
-            anom_raster = gdal.Open(basename, GA_ReadOnly)
+            anom_raster = gdal.Open(r, GA_ReadOnly)
             anom_band = anom_raster.GetRasterBand(1)
+            anom_gt, anom_proj = anom_raster.GetGeoTransform(), anom_raster.GetProjection()
             anom_cols, anom_rows = anom_raster.RasterXSize, anom_raster.RasterYSize
             anom_ds = anom_band.ReadAsArray(0, 0, anom_cols, anom_rows)
+
+            topleft_x = anom_gt[0]
+            topleft_y = anom_gt[3]
+            pix_width = anom_gt[1]
+            pix_height = anom_gt[5]*-1
+
+            for location in loc_list:
+                pix_x = (loc_list[location][0][0] - topleft_x)/pix_width
+                pix_y = (loc_list[location][0][1] - topleft_y)/pix_height
+                print location, pix_x, pix_y, anom_ds[pix_y, pix_x]
         else:
             pass
 
